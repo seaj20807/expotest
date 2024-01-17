34f9d868398ee12c486e28fcac4ae52a382f1db5
Sampo Jaakkola <sampo.jaakkola@myy.haaga-helia.fi>
2024-01-17 17:53:57 +0200

    First test for mobile programming course

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+
+  const [message, setMessage] = useState('');
+
+  const showAlert = () => {
+    Alert.alert('Hello', 'Syötit tekstin: ' + message);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput 
+        style={{ width: 200, borderColor: 'grey', borderWidth: 1 }}
+        onChangeText={ text => setMessage(text) } 
+      />
+      <Button title="Press me" onPress={showAlert} />
       <StatusBar style="auto" />
     </View>
   );
